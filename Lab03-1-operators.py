27aa27b93a54731af1900662cf250682e35cd6e3
Your Name <you@example.com>
2020-02-04 00:30:10 +0000

    lab03-1 assignment

---
+++ Lab03-1-operators.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+42 = n
+
+
+# In[9]:
+
+
+x = y = 1
+
+
+# In[10]:
+
+
+x
+
+
+# In[26]:
+
+
+n = "42"；
+
+
+# In[27]:
+
+
+n
+
+
+# In[28]:
+
+
+x = 2
+y = 3
+
+
+# In[30]:
+
+
+xy
+
+
+# In[31]:
+
+
+import math
+def v(n):
+    return 4/3*math.pi*n**3
+
+
+# In[32]:
+
+
+v(5)
+
+
+# In[33]:
+
+
+math.pi
+
+
+# In[34]:
+
+
+cost = 24.95*0.6*60 + 3 +0.75*59
+
+
+# In[35]:
+
+
+cost
+
+
+# In[37]:
+
+
+11/2
+
+
+# In[38]:
+
+
+math.floor(5.6)
+
+
+# In[58]:
+
+
+min = (8 + 15/60)*2 +(7 + 12/60)*3
+
+
+# In[59]:
+
+
+min
+
+
+# In[40]:
+
+
+min1 = 6*60 +52
+
+
+# In[54]:
+
+
+min1
+
+
+# In[60]:
+
+
+minend = min + min1
+
+
+# In[61]:
+
+
+minend
+
+
+# In[62]:
+
+
+hour = math.floor(minend/60)
+
+
+# In[63]:
+
+
+hour
+
+
+# In[83]:
+
+
+m = round(minend - hour*60)
+
+
+# In[84]:
+
+
+m
+
+
+# In[85]:
+
+
+print("Time for breakfast is: %d : %d" % (hour, m))
+
+
+# In[82]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
